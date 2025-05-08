e27bf621d0ac3595c2b3476b089b6385abe74155
Vinicius Pedrinho <vrpedrinho@gmail.com>
2025-05-08 10:33:28 -0300

    Display services list into settings

--- lib/screens/settings_screen/services_settings.dart
+++ lib/screens/settings_screen/services_settings.dart
@@ -1,5 +1,7 @@
 import 'package:byewall3/ui/components/custom_sliverappbar.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:byewall3/break_services/services_model.dart';
 
 class ServiceSettingsView extends StatelessWidget {
   final ScrollController controller;
@@ -10,10 +12,45 @@ class ServiceSettingsView extends StatelessWidget {
   Widget build(BuildContext context) {
     final customSliverAppBar = CustomSliverAppBar(context);
 
-    return CustomScrollView(
-      key: const PageStorageKey('serviceSettings'),
-      controller: controller,
-      slivers: [customSliverAppBar.buildSliverAppBar('services')],
+    return FutureBuilder<Box<ServicesModel>>(
+      future: Hive.openBox<ServicesModel>('services'),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+
+        final box = snapshot.data!;
+        final services =
+            box.values.toList()
+              ..sort((a, b) => a.serviceName.compareTo(b.serviceName));
+
+        return CustomScrollView(
+          key: const PageStorageKey('serviceSettings'),
+          controller: controller,
+          slivers: [
+            customSliverAppBar.buildSliverAppBar('services'),
+            SliverList(
+              delegate: SliverChildBuilderDelegate((
+                BuildContext context,
+                int index,
+              ) {
+                final service = services[index];
+                return ListTile(
+                  title: Text(service.serviceName),
+                  subtitle: Text(service.serviceUrl),
+                  trailing: Text(
+                    service.dateAdd.toLocal().toString().split(' ')[0],
+                  ),
+                );
+              }, childCount: services.length),
+            ),
+          ],
+        );
+      },
     );
   }
 }
